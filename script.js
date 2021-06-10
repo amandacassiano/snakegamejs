c3095f26bbb00e3e86645b90b4c32e9a48cc6641
Amanda Cassiano <amandacassia505@gmail.com>
2021-06-10 13:02:19 -0300

    joguinho finalizado

--- script.js
+++ script.js
@@ -13,19 +13,19 @@ let food = {
 }
 
 function criarBG() {
-    context.fillStyle = "lightgreen";
+    context.fillStyle = "#7FFFD4";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 function criarCobrinha(){
     for(i=0; i < snake.length; i++){
-        context.fillStyle = "green";
+        context.fillStyle = "#FF1493";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
 function drawFood(){
-    context.fillStyle = "red"
+    context.fillStyle = "#FFFF00";
     context.fillRect(food.x, food.y, box, box);
 }
 document.addEventListener('keydown', update);
